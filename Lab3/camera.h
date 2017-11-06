96e8df461d1555139d6e62cc15610cf80aa31e1d
Andy MacGregor <Andrew_MacGregor@student.uml.edu>
2017-11-06 12:10:40 -0500

    added interface for camera and temp sensor

---
+++ Lab3/camera.h
@@ -0,0 +1,7 @@
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
+bool capture_and_save_image(char* filename);
+
+#endif
---
+++ Lab3/temp_sensor.h
@@ -0,0 +1,8 @@
+
+#ifndef TEMP_SENSOR_H
+#define TEMP_SENSOR_H
+
+bool init_temp_sensor();
+double get_temp_in_f();
+
+#endif
