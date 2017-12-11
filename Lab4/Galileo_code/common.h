ac53e911c1e84ef1e6320bbd593b49a30f6fcbb6
xXIXxLovexXMemesXx <31903905+xXIXxLovexXMemesXx@users.noreply.github.com>
2017-12-11 13:43:35 -0500

    Updated Galileo code with Thread 1 functionality

--- Lab4/Galileo_code/common.h
+++ Lab4/Galileo_code/common.h
@@ -6,6 +6,29 @@
 //constants
 #define MAX_FILENAME            256
 #define DEFAULT_FILENAME "No_face_detected"
+#define TEMPERATURE_REGISTER 0x00	//According to table 1 in the TMP102 Datasheet
+#define CONFIG_REGISTER 0x01	  	//These values will determine what register we are
+#define T_LOW_REGISTER 0x02			//communicating with..not sure if we them.....
+#define T_HIGH_REGISTER 0x03
+
+#define TMP102Address 0x48
+using namespace mraa;
+
+double temperature_threshold = 30;
+double get_temp(); // temperature in C
+
+double get_temp() {
+	I2c i2c(0);
+	i2c.address(TMP102Address);
+
+	uint8_t dataReg[2];
+
+	int buffer = i2c.read(dataReg, 2); // read two bytes from the registers
+
+	int temperature = ((dataReg[0] << 8 | dataReg[1]) >> 4);
+
+	return temperature*0.0625;
+}
 
 //structure to hold data that is shared between threads
 typedef struct {
